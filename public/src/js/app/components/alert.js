918deb607461d3f4912c164aa80730e2f251f72c
John <john.grindall@gmail.com>
2014-08-15 10:41:28 +0100

    alert popup

---
+++ public/src/js/app/components/alert.js
@@ -0,0 +1,44 @@
+
+define(['app/components/buttons/tickbutton', 'app/game', 'app/components/container', 'app/utils/textfactory', 'app/components/abstractpopup'],
+
+function(TickButton, Game, Container, TextFactory, AbstractPopup){
+	
+	"use strict";
+		
+	var Alert = function(options){
+		options.bgasset = 'alert';
+		AbstractPopup.call(this, options);
+	};
+	
+	Alert.prototype = Object.create(AbstractPopup.prototype);
+	Alert.prototype.constructor = Alert;
+	
+	Alert.WIDTH = 400;
+	Alert.HEIGHT = 200;
+	
+	Alert.prototype.addOk = function () {
+		var x, y, b;
+		x = Game.cx() - TickButton.WIDTH/2;
+		y = this.bounds.y + this.bounds.h - TickButton.HEIGHT;
+		b = new TickButton({"x":x, "y":y});
+		b.create();
+		b.mouseUpSignal.add(this.buttonUp, this);
+		this.group.add(b.sprite);
+		this.buttons.push(b);
+	};
+	
+	Alert.prototype.addButtons = function () {
+		AbstractPopup.prototype.addButtons.call(this);
+		this.addOk();
+	};
+	
+	return Alert;
+	
+});
+	
+
+
+
+
+
+
